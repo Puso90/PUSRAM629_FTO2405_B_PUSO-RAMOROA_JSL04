7b35657404830454665b01f925bb6ff0e58bf947
Puso90 <juwishbrands@gmail.com>
2024-07-19 02:18:30 +0200

    Challenge 5: Late Night Coding Session

--- index.js
+++ index.js
@@ -62,15 +62,17 @@ if (currentTemp <= 5) {
 // Define the current hour in 24-hour format
 let currentHour = 22; 
 
-// Write your code below this line to determine if it's a good idea to have a Monster Energy drink based on the current hour
-// If it's between 7am and midnight (24-hour format), print "Unleash the beast!"
-// Otherwise, print "Better stick to water."
+ let message = (currentHour > 7 && currentHour < 24 ) ? `Unleash the beast!` : `Better stick to water.`;
+ console.log(message);
 
-
-
+// Shorthand ternary operator
+/*
+let age = 20;
+let message = (age >= 18) ? 'You are an adult.' : 'You are a minor.';
+console.log(message); */
 
 
 
 // In each of these challenges, the ternary operator is used to decide between two options based on a condition, 
 // demonstrating its utility for concise conditional logic. Remember that while ternaries can make the code more succinct, 
-// they should be used judiciously to maintain readability, especially for more complex conditions.
+// they should be used judiciously to maintain readability, especially for more complex conditions
